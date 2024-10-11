5f51311950cd99a5b8b6bf0a6bb82d757d3ddd6e
Brett Odgers-Jewell <brett.odgers-jewell@konjur.sg>
2024-10-11 08:10:35 +0800

    updated module

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "instrumentation_key" {
 output "app_id" {
   value = azurerm_application_insights.app_insights.app_id
 }
+
+output "app_insights_output" {
+  value = azurerm_application_insights.app_insights
+}
